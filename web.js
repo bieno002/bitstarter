5a868099dd057e2ac0dd3b7159143dd44c7dd3ff
bieno002 <jerrad.bieno@gmail.com>
2013-07-07 21:58:27 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var string = fs.readFileSync('index.html');
-    response.send(string);
+    response.send(Buffer.toString(string));
 });
 
 var port = process.env.PORT || 5000;
